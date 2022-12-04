c84d530bed1875890dbaa0cca490f8bb6a16b09e
nahid <nahid7us@gmail.com>
2022-12-04 13:22:53 -0500

    2nd commit - added main

--- src/com/qa/fifa/Main.java
+++ src/com/qa/fifa/Main.java
@@ -3,8 +3,9 @@ package com.qa.fifa;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("i addes a new class in this project");
+		
 	}
 
 }
